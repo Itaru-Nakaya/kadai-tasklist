9e9333bc78bcffbb2252f21ff2191520b3741b09
itaru.nakaya <george_h_taro@yahoo.co.jp>
2020-06-28 13:09:27 +0000

    課題用

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,61 @@
+class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    if logged_in?
+      @task = current_user.tasks.build  
+      @tasks = Task.all
+    end
+  end
+  
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = current_user.tasks.build(task_params)
+    
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
+    else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task.destroy
+    
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+end
